efb8b6b77a54308046fbea57ad78355254dea494
Nilesh Laxmi <nileshlaxmi4@gmail.com>
2019-02-12 23:34:49 +0530

    updated design of to-do-list box

--- to-do-list-redux-demo/src/App.js
+++ to-do-list-redux-demo/src/App.js
@@ -5,16 +5,20 @@ import AddTask from './container/AddTask';
 import ListItem from './container/LisItem';
 import Header from './component/Header';
 
+
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <Header />
-        <AddTask />
-        <ul>
-          <ListItem />
-        </ul>
+      <div className="body-div">
+        <div className="App">
+          <Header />
+          <AddTask />
+          <ul>
+            <ListItem />
+          </ul>
+        </div>
       </div>
+      
     );
   }
 }
